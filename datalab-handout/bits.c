f4e140400f153ed5b2dd723f72d3d74c922fabcb
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2019-04-23 19:27:41 -0700

    Add files via upload

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -1,7 +1,8 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * Prithvi Kannan
+ * 405110096
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -132,14 +133,15 @@ NOTES:
 
 #endif
 /*
- * bitParity - returns 1 if x contains an odd number of 0's
- *   Examples: bitParity(5) = 0, bitParity(7) = 1
- *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 20
- *   Rating: 4
- */
+* bitParity - returns 1 if x contains an odd number of 0's
+*   Examples: bitParity(5) = 0, bitParity(7) = 1
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 20
+*   Rating: 4
+*/
 int bitParity(int x) {
-  // ^ operator keeps same parity, so we half until we only have 1 bit, then compare with something we know has odd zeros
+  /* ^ operator keeps same parity, so we half until we only have 1 bit, then compare with something 
+  we know has odd zeros */
   x = x ^ (x >> 16);
   x = x ^ (x >> 8);
   x = x ^ (x >> 4);
@@ -147,136 +149,156 @@ int bitParity(int x) {
   x = x ^ (x >> 1);
   return (x & 1);
 }
+
 /* 
- * rotateRight - Rotate x to the right by n
- *   Can assume that 0 <= n <= 31
- *   Examples: rotateRight(0x87654321,4) = 0x18765432
- *   Legal ops: ~ & ^ | + << >> !
- *   Max ops: 25
- *   Rating: 3 
- */
+* rotateRight - Rotate x to the right by n
+*   Can assume that 0 <= n <= 31
+*   Examples: rotateRight(0x87654321,4) = 0x18765432
+*   Legal ops: ~ & ^ | + << >> !
+*   Max ops: 25
+*   Rating: 3 
+*/
 int rotateRight(int x, int n) {
-  // 32-n = 32 + ~n + 1
-  int amtMove = x << (32 + (~n +1));
+  /* We use 2s complement arithmetic to represent the amount we want to shift, 32-n = 32 + ~n + 1 */
+  int amtToMove = x << (32 + (~n +1));
   int max = (~(1 << 31)) >> (n+((~1)+1));
   x = x >> n;
   x = x & max;
-  return x | amtMove;
+  return x | amtToMove;
 }
+
 /* 
- * byteSwap - swaps the nth byte and the mth byte
- *  Examples: byteSwap(0x12345678, 1, 3) = 0x56341278
- *            byteSwap(0xDEADBEEF, 0, 2) = 0xDEEFBEAD
- *  You may assume that 0 <= n <= 3, 0 <= m <= 3
- *  Legal ops: ! ~ & ^ | + << >>
- *  Max ops: 25
- *  Rating: 2
- */
+* byteSwap - swaps the nth byte and the mth byte
+*  Examples: byteSwap(0x12345678, 1, 3) = 0x56341278
+*            byteSwap(0xDEADBEEF, 0, 2) = 0xDEEFBEAD
+*  You may assume that 0 <= n <= 3, 0 <= m <= 3
+*  Legal ops: ! ~ & ^ | + << >>
+*  Max ops: 25
+*  Rating: 2
+*/
 int byteSwap(int x, int n, int m) {
-	int maskM = 255 << (m<<3);
-	int maskN = 255 << (n<<3);
+  /* This function determines how much needs to be shifted based on the input to the byteswap, then creates "masks"
+  that we use to denote which parts are changing. We apply the masks with the and operator. */
+  
+  int maskM = 0xFF << (m<<3);
+  int maskN = 0xFF << (n<<3);
+
   int swapM = maskM & x;
   int swapN = maskN & x;
-	int mask = ~(maskM | maskN);
-  int toM, toN, switched;
-	x = x & mask;
-	toM = ((swapN>>(n << 3)) & 255) << (m << 3);
-	toN = ((swapM>>(m << 3)) & 255) << (n << 3);
-	switched = toM | toN;
-	return x + switched;
+
+  int mask = ~(maskM | maskN);
+  int toM, toN, afterSwitch;
+  x = x & mask;
+  toM = ((swapN>>(n << 3)) & 0xFF) << (m << 3);
+  toN = ((swapM>>(m << 3)) & 0xFF) << (n << 3);
+  afterSwitch = toM | toN;
+  return x + afterSwitch;
 }
+
 /* 
- * fitsShort - return 1 if x can be represented as a 
- *   16-bit, two's complement integer.
- *   Examples: fitsShort(33000) = 0, fitsShort(-32768) = 1
- *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 8
- *   Rating: 1
- */
+* fitsShort - return 1 if x can be represented as a 
+*   16-bit, two's complement integer.
+*   Examples: fitsShort(33000) = 0, fitsShort(-32768) = 1
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 8
+*   Rating: 1
+*/
 int fitsShort(int x) {
-  // left shift then right shift, compare to see if the value of x changed
+  /* left shift then right shift, compare to see if the value of x changed */
   return !(x ^ ((x << 16) >> 16)); 
 }
+
 /* 
- * bitAnd - x&y using only ~ and | 
- *   Example: bitAnd(6, 5) = 4
- *   Legal ops: ~ |
- *   Max ops: 8
- *   Rating: 1
- */
+* bitAnd - x&y using only ~ and | 
+*   Example: bitAnd(6, 5) = 4
+*   Legal ops: ~ |
+*   Max ops: 8
+*   Rating: 1
+*/
 int bitAnd(int x, int y) {
-  // !(x&y) = !x | !y
+  /* We use the 2s complement representation of integers to rearrange !(x&y) as !x | !y */
   return ~(~x|~y);
 }
+
 /* 
- * subOK - Determine if can compute x-y without overflow
- *   Example: subOK(0x80000000,0x80000000) = 1,
- *            subOK(0x80000000,0x70000000) = 0, 
- *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 20
- *   Rating: 3
- */
+* subOK - Determine if can compute x-y without overflow
+*   Example: subOK(0x80000000,0x80000000) = 1,
+*            subOK(0x80000000,0x70000000) = 0, 
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 20
+*   Rating: 3
+*/
 int subOK(int x, int y) {
-  // x - y = x + (~y + 1)
+  /* This code uses 2s complement arithmetic to represent x - y as x + (~y + 1). 
+  If x and y have same MSB, and after the sub gives a different MSB than x, then we have overflow */
+
   int a = x + ~y + 1;
-  // x and y have same MSB
   int b = x ^ y;
-  // sub gives different MSB than x
   a = a ^ x;
   return !((a & b) >> 31);
-  }
+}
+
 /* 
- * isGreater - if x > y  then return 1, else return 0 
- *   Example: isGreater(4,5) = 0, isGreater(5,4) = 1
- *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 24
- *   Rating: 3
- */
+* isGreater - if x > y  then return 1, else return 0 
+*   Example: isGreater(4,5) = 0, isGreater(5,4) = 1
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 24
+*   Rating: 3
+*/
 int isGreater(int x, int y) {
+  /* this code compares the MSB of x and y to see if one is greater than the other. If x positive by y negative,
+  we know x is greater. If that is not the case, we make sure x isn't negative when y is positive and return the value*/
   int var1 = !((x + ~y) >> 31);
   x = x >> 31;
   y = y >> 31;
   return ((!x) & y) | (var1 & ((!x) | y));
 }
+
 /* 
- * fitsBits - return 1 if x can be represented as an 
- *  n-bit, two's complement integer.
- *   1 <= n <= 32
- *   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
- *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 15
- *   Rating: 2
- */
+* fitsBits - return 1 if x can be represented as an 
+*  n-bit, two's complement integer.
+*   1 <= n <= 32
+*   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 15
+*   Rating: 2
+*/
 int fitsBits(int x, int n) {
-  // want to get 32 - x
-  // ~x = -x - 1 so -x = ~x + 1
-  // 32 + (~x + 1) = 33 + ~x  
+  /* This is similar to fitsShort, where we shift left and then right and compare the outputs.
+  We want to get 32 - x, which is the amount we want to shift by using 2s complement arithmetic. We know 
+  ~x = -x - 1 so -x = ~x + 1, and therefore 32 + (~x + 1) = 33 + ~x */
   int amt = 33 + ~n;
   return !(x ^ ((x << amt) >> amt));
 }
+
 /* 
- * negate - return -x 
- *   Example: negate(1) = -1.
- *   Legal ops: ! ~ & ^ | + << >>
- *   Max ops: 5
- *   Rating: 2
- */
+* negate - return -x 
+*   Example: negate(1) = -1.
+*   Legal ops: ! ~ & ^ | + << >>
+*   Max ops: 5
+*   Rating: 2
+*/
 int negate(int x) {
-  // ~x = -x - 1
+  /* We use the 2s complement representation of ~x as -x - 1 and rearrange to produce negative x */
   return ~x + 1;
 }
+
 /*
- * isTmax - returns 1 if x is the maximum, two's complement number,
- *     and 0 otherwise 
- *   Legal ops: ! ~ & ^ | +
- *   Max ops: 10
- *   Rating: 1
- */
+* isTmax - returns 1 if x is the maximum, two's complement number,
+*     and 0 otherwise 
+*   Legal ops: ! ~ & ^ | +
+*   Max ops: 10
+*   Rating: 1
+*/
 int isTmax(int x) {
-    int newX = x  + 1;
-    x = x + newX;
-    newX = !(newX);
-    x = ~x;
-    x = x | newX;
-    return !x;
+  /* this function adds 1 to x to see if it will overflow the int, which would be indicated by a 1 in the MSB.
+  We do this by flipping x and and applying the not operator to the x+1 term. Then we compare x to the newX term 
+  and return the oppposite of the output.*/
+
+  int newX = x  + 1;
+  x = x + newX;
+  newX = !(newX);
+  x = ~x;
+  x = x | newX;
+  return !x;
 }
