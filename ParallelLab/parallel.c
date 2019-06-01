4b6d80e0f69a26c442c84e4d1dd46023034a7dc0
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2019-06-01 09:56:56 -0700

    Add files via upload

--- ParallelLab/parallel.c
+++ ParallelLab/parallel.c
@@ -12,94 +12,109 @@
 #include "utils.h"
 
 void work_it_par(long *old, long *new) {
-  int i, j, k;
-  int u, v, w;
-  int tempi, tempj, tempk, tempu, tempv;
-  long compute_it, compute_it1;
-  long aggregate=1.0;
-  int temp = 0;
-  int ii,jj,kk;
-  int SIZE = DIM-1;
-  int index;
-  int BLOCK = 8;
+    int i, j, k;
+    long u = 0;
+    long u_0, u_1, u_2, u_3, u_4, u_5 ,u_6, u_7, u_8, u_9 = 0;
+    long tempSum = 0;
+    const long DIM_2 = DIM * DIM;
+    long compute_it;
+    long aggregate = 1.0;
+    long address = 0;
+    long tempAddress = 0;
 
-  #pragma omp parallel for private (jj,kk,i,j,k,u,v,w,index,compute_it1,tempi, tempj, tempu, tempv) reduction (+: aggregate)
-  for (ii = 1; ii < SIZE; ii+= BLOCK) {
-    for (jj = 1; jj < SIZE; jj+= BLOCK) {
-      for (kk = 1; kk < SIZE; kk+= BLOCK) {
-        int iiSize = ii+BLOCK;
-        for (i=ii; (i<iiSize && i<SIZE); i++) {
-          tempi = i*DIM*DIM;
-          int jjSize = jj+BLOCK;
-          for (j=jj; (j<jjSize && j<SIZE); j++) {
-            tempj = tempi + j*DIM;  
-            int kkSize = kk+BLOCK;
-            for (k=kk; (k<kkSize && k<SIZE); k++) {
-              index = tempj + k;
-              //index = i*DIM*DIM + j*DIM + k;
-              new[index] = 0;
-              compute_it1=old[index] * we_need_the_func();
-              aggregate += compute_it1 / gimmie_the_func();
-              for (u=-1; u<=1; u++) {
-                tempu = (i+u)*DIM*DIM;
-                for (v=-1; v<=1; v++) {
-                  tempv = (j+v)*DIM;
-                  for (w=-1; w<=1; w++) {
-                    temp+=old[tempu+tempv+(k+w)];
-                  }
-                }
-              }
+    #pragma omp parallel for private(j,k,address, compute_it, tempAddress, u) reduction(+: aggregate) reduction(+:tempSum)\
+    reduction(+:u_0) reduction(+:u_1) reduction(+:u_2) reduction(+:u_3) reduction(+:u_4) reduction(+:u_5) reduction(+:u_6) reduction(+:u_7) reduction(+:u_8) reduction(+:u_9)
+    for (i=1; i<DIM-1; i++) {
+        for (j=1; j<DIM-1; j++) {
+            for (k=1; k<DIM-1; k++) {
+                address = i * DIM_2 + j * DIM + k;
+                compute_it = old[address] * we_need_the_func();
+                aggregate += compute_it / gimmie_the_func();
+                tempSum = 0;
+
+                tempAddress = address - DIM_2;
+
+                tempAddress -= DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempAddress += DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempAddress += DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+
+                tempAddress = address;
+
+                tempAddress -= DIM;
+
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempAddress += DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempAddress += DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+
+                tempAddress = address + DIM_2;
+
+                tempAddress -= DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempAddress += DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempAddress += DIM;
+                tempSum += old[tempAddress-1];
+                tempSum += old[tempAddress];
+                tempSum += old[tempAddress+1];
+
+                tempSum /= 27;
+                new[address] = tempSum;
+                u = (tempSum/100);
+                if (u<=0) u_0++;
+                if (u==1) u_1++;
+                if (u==2) u_2++;
+                if (u==3) u_3++;
+                if (u==4) u_4++;
+                if (u==5) u_5++;
+                if (u==6) u_6++;
+                if (u==7) u_7++;
+                if (u==8) u_8++;
+                if (u>=9) u_9++;
             }
-          }
         }
-      }
     }
-  }
-
-  printf("AGGR:%ld\n",aggregate);
-
-  for (i=1; i<DIM-1; i++) {
-    for (j=1; j<DIM-1; j++) {
-      for (k=1; k<DIM-1; k++) {
-        u=(new[i*DIM*DIM+j*DIM+k]/100);
-        if (u<=0) u=0;
-        if (u>=9) u=9;
-        histogrammy[u]++;
-      }
-    }
-  }
   
-// //#pragma omp parallel num_threads(4) private(i,j,k,u,v,w, tempi, tempj, tempk,compute_it)
-//   for (i=1; i<DIM-1; i++) {
-//     tempi = i*DIM*DIM;
-//     for (j=1; j<DIM-1; j++) {
-//       tempj = tempi + j*DIM;
-//       for (k=1; k<DIM-1; k++) {
-//         temp = 0;
-//         tempk = tempj + k;
-//         //#pragma omp for nowait reduction(+:temp)
-//         for (u=-1; u<=1; u++) {
-//           for (v=-1; v<=1; v++) {
-//             for (w=-1; w<=1; w++) {
-//                temp+=old[(i+u)*DIM*DIM+(j+v)*DIM+(k+w)];
-//             }
-//           }
-//         }
+    printf("AGGR:%ld\n",aggregate);
 
-//         temp/=27;
-//         new[tempk]=temp;
-//         //#pragma omp single nowait
-//         u=(temp/100);        
-//         //#pragma omp single nowait
-//         if (u<=0) u=0;
-//         //#pragma omp single nowait
-//         if (u>=9) u=9;
-//         //#pragma omp single nowait
-//         histogrammy[u]++;
-//       }
-//     }
-//   }
-
-// }
+    histogrammy[0] = u_0;
+    histogrammy[1] = u_1;
+    histogrammy[2] = u_2;
+    histogrammy[3] = u_3;
+    histogrammy[4] = u_4;
+    histogrammy[5] = u_5;
+    histogrammy[6] = u_6;
+    histogrammy[7] = u_7;
+    histogrammy[8] = u_8;
+    histogrammy[9] = u_9;
 
 }
+
